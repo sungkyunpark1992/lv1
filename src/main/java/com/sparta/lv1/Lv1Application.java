f98c3a6a948a1dfe472e1bedf10551b503ada93a
LOGOS125-GALAXY\qkrtj <qkrtjdrbs125@naver.com>
2023-07-07 09:18:42 +0900

    init commit

---
+++ src/main/java/com/sparta/lv1/Lv1Application.java
@@ -0,0 +1,13 @@
+package com.sparta.lv1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lv1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Lv1Application.class, args);
+	}
+
+}
